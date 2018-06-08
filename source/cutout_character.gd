f8027e57acaec30f3feb1d26a2abb40a92032690
Henrique Campos <henriiquecampos@gmail.com>
2018-06-08 10:11:40 -0300

    Updated Template character

---
+++ source/cutout_character.gd
@@ -0,0 +1,9 @@
+onready var initial_scale = get_scale()
+
+enum directions {RIGHT, LEFT}
+
+func flip_sprites(direction):
+	if direction == RIGHT:
+		scale.x = initial_scale.x
+	else:
+		scale.x = initial_scale.x * -1
